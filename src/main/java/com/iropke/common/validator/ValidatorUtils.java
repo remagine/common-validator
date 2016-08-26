031ccda623bbd1ccc41b6f7474493c7d7d00c981
arthur <arthur@iropke.com>
2016-08-26 19:02:48 +0900

    validator test class add

--- src/main/java/com/iropke/common/validator/ValidatorUtils.java
+++ src/main/java/com/iropke/common/validator/ValidatorUtils.java
@@ -8,18 +8,18 @@ import java.util.regex.Pattern;
 public class ValidatorUtils {
 
     /**
-     * String 최소값 validato, 띄어쓰기 제외
+     * String 최소값 validato, 앞뒤공백 제거(src.trim())
      */
     public static boolean length(String src, int min){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
         return src.trim().length() >= min;
     }
 
     /**
-     *  String 최소값 최대값 validator, 띄어쓰기 제외
+     *  String 최소값 최대값 validator, 앞뒤공백 제거(src.trim())
      */
     public static boolean length(String src, int min, int max){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
         return src.trim().length() >= min && src.trim().length() <= max;
     }
 
@@ -27,51 +27,51 @@ public class ValidatorUtils {
      * Email 형식 validator
      */
     public static boolean email(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
-                "^[a-z0-9A-Z_-]*@[a-z0-9A-Z]*.[a-zA-Z.]*$",
+                "[\\w\\~\\-\\.]+@[\\w\\~\\-]+(\\.[\\w\\~\\-]+)+",
                 src);
     }
 
     /**
-     * 일반전화 형식 validator
+     * 일반전화 형식 validator, 0#(#)-###(#)-#### , -포함
      */
     public static boolean phone(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
-                "^\\d{2,3}\\d{3,4}\\d{4}$",
+                "^0\\d{1,2}-\\d{3,4}-\\d{4}$",
                 src);
     }
 
     /**
-     * 핸드폰 형식 validator
+     * 핸드폰 형식 validator, 01#-####-#### , -포함
      */
     public static boolean mobile(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
-                "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$",
+                "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$",
                 src);
     }
 
     /**
-     * 영문(대소문자) 형식 validator, , 띄어쓰기 제외
+     * 영문(대소문자) 형식 validator, 공백문자불가 , 앞뒤공백 제거(src.trim())
      */
     public static boolean alpha(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
-                "^[a-zA-Z]*$",
+                "^[a-zA-Z]+$",
                 src.trim());
     }
 
     /**
-     * 영문(소문자) 형식 validator, , 띄어쓰기 제외
+     * 영문(소문자) 형식 validator, 공백문자불가 , 앞뒤공백 제거(src.trim())
      */
     public static boolean alphaLower(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[a-z]*$",
@@ -79,10 +79,10 @@ public class ValidatorUtils {
     }
 
     /**
-     * 영문(대문자) 형식 validator, , 띄어쓰기 제외
+     * 영문(대문자) 형식 validator, 공백문자불가 , 앞뒤공백 제거(src.trim())
      */
     public static boolean alphaUpper(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[A-Z]*$",
@@ -91,10 +91,10 @@ public class ValidatorUtils {
 
 
     /**
-     * 영문(대소문자) + 숫자 형식 validator, , 띄어쓰기 제외
+     * 영문(대소문자) + 숫자 형식 validator, 공백문자불가 , 앞뒤공백 제거(src.trim())
      */
     public static boolean alphanum(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[0-9a-zA-Z]*$",
@@ -102,10 +102,10 @@ public class ValidatorUtils {
     }
 
     /**
-     * 영문(소문자) + 숫자 형식 validator, , 띄어쓰기 제외
+     * 영문(소문자) + 숫자 형식 validator, 공백문자불가 , 앞뒤공백 제거(src.trim())
      */
     public static boolean alphanumLower(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[0-9a-z]*$",
@@ -113,10 +113,10 @@ public class ValidatorUtils {
     }
 
     /**
-     * 영문(대문자) + 숫자 형식 validator, , 띄어쓰기 제외
+     * 영문(대문자) + 숫자 형식 validator, 공백문자불가 , 앞뒤공백 제거(src.trim())
      */
     public static boolean alphanumUpper(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[0-9A-Z]*$",
@@ -124,10 +124,10 @@ public class ValidatorUtils {
     }
 
     /**
-     *  ID 형식 validator, 영문(대소문자) + 숫자 + "_"
+     *  ID 형식 validator, 공백문자불가 , 영문(대소문자) + 숫자 + "_"
      */
     public static boolean identifier(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[0-9a-zA-Z_]*$",
@@ -135,10 +135,10 @@ public class ValidatorUtils {
     }
 
     /**
-     *  ID 형식 validator, 영문(소문자) + 숫자 + "_"
+     *  ID 형식 validator, 공백문자불가 , 영문(소문자) + 숫자 + "_"
      */
     public static boolean identifierLower(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[0-9a-z_]*$",
@@ -146,10 +146,10 @@ public class ValidatorUtils {
     }
 
     /**
-     *  ID 형식 validator, 영문(대문자) + 숫자 + "_"
+     *  ID 형식 validator, 공백문자불가 , 영문(대문자) + 숫자 + "_"
      */
     public static boolean identifierUpper(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[0-9A-Z_]*$",
@@ -157,10 +157,10 @@ public class ValidatorUtils {
     }
 
     /**
-     *  CSS 형식 validator, 영문(대소문자) + 숫자 + "_" + "-"
+     *  CSS 형식 validator, 공백문자불가 , 영문(대소문자) + 숫자 + "_" + "-"
      */
     public static boolean cssClassName(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[0-9A-Z_-]*$",
@@ -168,10 +168,10 @@ public class ValidatorUtils {
     }
 
     /**
-     *  숫자 형식 validator, 앞에 0이 붙어도 상관없음(오직 양수만)
+     *  숫자 형식 validator, 공백문자불가 , 앞에 0이 붙어도 상관없음(오직 양수만)
      */
     public static boolean number(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[0-9]*$",
@@ -182,7 +182,7 @@ public class ValidatorUtils {
      *   정수 숫자 형식 validator, 음수 - 0 - 양수 (단 0으로 시작하는 2자리 이상 수는 false)
      */
     public static boolean integerNumber(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[-]?(0|[1-9][0-9]*)*$",
@@ -193,7 +193,7 @@ public class ValidatorUtils {
      *   실수 숫자 형식 validator, 음수 - 0 - 양수 (단 0으로 시작하는 2자리 이상 수는 false, 숫자 자리수 제한 X)
      */
     public static boolean floatNumber(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
         return Pattern.matches(
                 "^[-]?(0|[1-9][0-9]*)(\\.[0-9]+)$",
@@ -205,7 +205,7 @@ public class ValidatorUtils {
      *   실수 숫자 형식 validator, 음수 - 0 - 양수 (단 0으로 시작하는 2자리 이상 수는 false, 숫자 자리수 제한 있음)
      */
     public static boolean floatNumber(String src, int size){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
         if (src.length() > size) return false;
 
         return Pattern.matches(
@@ -217,7 +217,7 @@ public class ValidatorUtils {
      *   우편변호 형식 validator, ###-### 구우편번호 or 5자리 신우편번호
      */
     public static boolean zipcode(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
 
         return Pattern.matches(
@@ -230,11 +230,12 @@ public class ValidatorUtils {
      *   url 형식 validator, http:// & https://
      */
     public static boolean url(String src){
-        if (src==null) return false;
+        if (src==null || src.trim().length() == 0) return false;
 
 
         return Pattern.matches(
-                "^\\d{3}-\\d{3}|\\d{5}$",
+                "^()"
+                + "$",
                 src);
     }
 
--- src/test/java/com/iropke/common/validator/ValidatorBasicTest.java
+++ src/test/java/com/iropke/common/validator/ValidatorBasicTest.java
@@ -11,7 +11,128 @@ public class ValidatorBasicTest {
     @Test
     public void testLengthMin() {
         Assert.assertTrue("min length is 3", ValidatorUtils.length("sss", 3));
+        Assert.assertTrue("min length is 3", ValidatorUtils.length("sss     ", 3));
+        Assert.assertTrue("min length is 3", ValidatorUtils.length("s s s     ", 3));
         Assert.assertFalse("min length is 3 fail", ValidatorUtils.length("ss", 3));
+        Assert.assertFalse("min length is 3 fail", ValidatorUtils.length("ss    ", 3));
+        Assert.assertFalse("min length is 3 fail", ValidatorUtils.length("     ", 3));
     }
 
+    @Test
+    public void testLengthMinMax() {
+        Assert.assertTrue("min length is 3 max length is 10", ValidatorUtils.length("sss", 3, 10));
+        Assert.assertTrue("min length is 3 max length is 10", ValidatorUtils.length("sss     ", 3, 10));
+        Assert.assertTrue("min length is 3 max length is 10", ValidatorUtils.length("1234567890", 3, 10));
+        Assert.assertTrue("min length is 3 max length is 10", ValidatorUtils.length("1234567890      ", 3, 10));
+        Assert.assertTrue("min length is 3 max length is 10", ValidatorUtils.length("14 5 6 790      ", 3, 10));
+        Assert.assertFalse("min length is 3 max length is 10 fail", ValidatorUtils.length("ss", 3, 10));
+        Assert.assertFalse("min length is 3 max length is 10 fail", ValidatorUtils.length("12345678901", 3, 10));
+        Assert.assertFalse("min length is 3 max length is 10 fail", ValidatorUtils.length("     ", 3, 10));
+    }
+
+    @Test
+    public void testEmail(){
+        Assert.assertTrue("email is remagine@naver.com" , ValidatorUtils.email("remagine@naver.com"));
+        Assert.assertTrue("email is remagine@naver.co.kr" , ValidatorUtils.email("remagine@naver.co.kr"));
+        Assert.assertTrue("email is 0remagine@naver.co.kr" , ValidatorUtils.email("0remagine@naver.co.kr"));
+        Assert.assertTrue("email is -_0remagine@naver.co.kr" , ValidatorUtils.email("-_0remagine@naver.co.kr"));
+        Assert.assertFalse("email is @_0remagine@naver.co.kr" , ValidatorUtils.email("@_0remagine@naver.co.kr"));
+        Assert.assertFalse("email is _0rem@agine@naver.co.kr" , ValidatorUtils.email("_0rem@agine@naver.co.kr"));
+        Assert.assertFalse("email is _0remaginenaver.co.kr" , ValidatorUtils.email("_0remaginenaver.co.kr"));
+    }
+
+    @Test
+    public void testPhone(){
+        Assert.assertTrue("phone is 02-123-1234" , ValidatorUtils.phone("02-123-1234"));
+        Assert.assertTrue("phone is 02-1231-1234" , ValidatorUtils.phone("02-1231-1234"));
+        Assert.assertTrue("phone is 023-123-1234" , ValidatorUtils.phone("023-123-1234"));
+        Assert.assertTrue("phone is 023-1233-1234" , ValidatorUtils.phone("023-1233-1234"));
+        Assert.assertFalse("phone is 0231-1233-1234" , ValidatorUtils.phone("0231-1233-1234"));
+        Assert.assertFalse("phone is 231-1233-1234" , ValidatorUtils.phone("231-1233-1234"));
+        Assert.assertFalse("phone is 023-12331-1234" , ValidatorUtils.phone("023-12331-1234"));
+        Assert.assertFalse("phone is 023-1233-12341" , ValidatorUtils.phone("023-1233-12341"));
+        Assert.assertFalse("phone is 02312332341" , ValidatorUtils.phone("02312332341"));
+        Assert.assertFalse("phone is 023-12-3-1234" , ValidatorUtils.phone("023-12-3-1234"));
+    }
+
+    @Test
+    public void testMobile(){
+        Assert.assertTrue("mobile is 010-123-1234" , ValidatorUtils.mobile("010-123-1234"));
+        Assert.assertTrue("mobile is 010-1231-1234" , ValidatorUtils.mobile("010-1231-1234"));
+        Assert.assertTrue("mobile is 011-1231-1234" , ValidatorUtils.mobile("011-1231-1234"));
+        Assert.assertTrue("mobile is 016-1231-1234" , ValidatorUtils.mobile("016-1231-1234"));
+        Assert.assertTrue("mobile is 017-1231-1234" , ValidatorUtils.mobile("017-1231-1234"));
+        Assert.assertTrue("mobile is 018-1231-1234" , ValidatorUtils.mobile("018-1231-1234"));
+        Assert.assertTrue("mobile is 019-1231-1234" , ValidatorUtils.mobile("019-1231-1234"));
+        Assert.assertFalse("mobile is 110-1231-1234" , ValidatorUtils.mobile("110-1231-1234"));
+        Assert.assertFalse("mobile is 01-1231-1234" , ValidatorUtils.mobile("01-1231-1234"));
+        Assert.assertFalse("mobile is 010-12311-1234" , ValidatorUtils.mobile("010-12311-1234"));
+        Assert.assertFalse("mobile is 010-1231-12341" , ValidatorUtils.mobile("010-1231-12341"));
+        Assert.assertFalse("mobile is 013-1231-1234" , ValidatorUtils.mobile("013-1231-1234"));
+        Assert.assertFalse("mobile is 01012311234" , ValidatorUtils.mobile("01012311234"));
+        Assert.assertFalse("mobile is 0 10-1231-1234" , ValidatorUtils.mobile("0 10-1231-1234"));
+        Assert.assertFalse("mobile is 010-1 231-1234" , ValidatorUtils.mobile("010-1 231-1234"));
+        Assert.assertFalse("mobile is 010-1231-1 234" , ValidatorUtils.mobile("010-1231-1 234"));
+        Assert.assertFalse("mobile is 010-12-31-1234" , ValidatorUtils.mobile("010-12-31-1234"));
+    }
+
+    @Test
+    public void testAlpha(){
+        Assert.assertTrue("alpha is aAbBcCdD" , ValidatorUtils.alpha("aAbBcCdD"));
+        Assert.assertTrue("alpha is aAbBcCdDzZ" , ValidatorUtils.alpha("aAbBcCdDzZ"));
+        Assert.assertTrue("alpha is     aAbBcCdDzZ" , ValidatorUtils.alpha("   aAbBcCdDzZ"));
+        Assert.assertTrue("alpha is     aAbBcCdDzZ      " , ValidatorUtils.alpha("   aAbBcCdDzZ      "));
+        Assert.assertFalse("alpha is aA bBc CdD" , ValidatorUtils.alpha("aA bBc CdD"));
+        Assert.assertFalse("alpha is 1aAbBcCdD" , ValidatorUtils.alpha("1aAbBcCdD"));
+        Assert.assertFalse("alpha is aAbBcCdD!" , ValidatorUtils.alpha("aAbBcCdD!"));
+        Assert.assertFalse("alpha is aAbBcCdDㄱ" , ValidatorUtils.alpha("1aAbBcCdDㄱ"));
+        Assert.assertFalse("alpha is 1aAbB3cCdD" , ValidatorUtils.alpha("1aAbB3cCdD"));
+        Assert.assertFalse("alpha is 1aAbB3cCdD" , ValidatorUtils.alpha(""));
+        Assert.assertFalse("alpha is 1aAbB3cCdD" , ValidatorUtils.alpha("       "));
+    }
+
+    @Test
+    public void testAlphaLower(){
+        Assert.assertTrue("alphaLower is abcd" , ValidatorUtils.alphaLower("abcd"));
+        Assert.assertTrue("alphaLower is    abcd" , ValidatorUtils.alphaLower("   abcd"));
+        Assert.assertTrue("alphaLower is abcd   " , ValidatorUtils.alphaLower("abcd   "));
+        Assert.assertTrue("alphaLower is abcdefgz" , ValidatorUtils.alphaLower("abcdefgz"));
+        Assert.assertTrue("alphaLower is c" , ValidatorUtils.alphaLower("c"));
+        Assert.assertFalse("alphaLower is abcd efgz" , ValidatorUtils.alphaLower("abcd efgz"));
+        Assert.assertFalse("alphaLower is abcd3fgz" , ValidatorUtils.alphaLower("abcd3fgz"));
+        Assert.assertFalse("alphaLower is abcdEfgz" , ValidatorUtils.alphaLower("abcdEfgz"));
+        Assert.assertFalse("alphaLower is " , ValidatorUtils.alphaLower(""));
+        Assert.assertFalse("alphaLower is     " , ValidatorUtils.alphaLower("    "));
+        Assert.assertFalse("alphaLower is   asd_df  " , ValidatorUtils.alphaLower(" asd_df   "));
+    }
+
+    @Test
+    public void testAlphaUpper(){
+        Assert.assertTrue("alphaUpper is ABCD" , ValidatorUtils.alphaUpper("ABCD"));
+        Assert.assertTrue("alphaUpper is ABCDEFGZ" , ValidatorUtils.alphaUpper("ABCDEFGZ"));
+        Assert.assertTrue("alphaUpper is ABCDEFGZ     " , ValidatorUtils.alphaUpper("ABCDEFGZ       "));
+        Assert.assertTrue("alphaUpper is      ABCDEFGZ" , ValidatorUtils.alphaUpper("       ABCDEFGZ"));
+        Assert.assertFalse("alphaUpper is ABCD EFGZ" , ValidatorUtils.alphaUpper("ABCD EFGZ"));
+        Assert.assertFalse("alphaUpper is ABCD3EFGZ" , ValidatorUtils.alphaUpper("ABCD3EFGZ"));
+        Assert.assertFalse("alphaUpper is ABCD!EFGZ" , ValidatorUtils.alphaUpper("ABCD!EFGZ"));
+        Assert.assertFalse("alphaUpper is ABCDhEFGZ" , ValidatorUtils.alphaUpper("ABCDhEFGZ"));
+        Assert.assertFalse("alphaUpper is ABCD3EFGZ" , ValidatorUtils.alphaUpper("ABCD_EFGZ"));
+        Assert.assertFalse("alphaUpper is " , ValidatorUtils.alphaUpper(""));
+        Assert.assertFalse("alphaUpper is    " , ValidatorUtils.alphaUpper("  "));
+    }
+
+    @Test
+    public void testAlphanum(){
+        Assert.assertTrue("alphanum is ABCD" , ValidatorUtils.alphanum("ABCD"));
+        Assert.assertTrue("alphanum is 1234" , ValidatorUtils.alphanum("1234"));
+        Assert.assertTrue("alphanum is asdf" , ValidatorUtils.alphanum("asdf"));
+        Assert.assertTrue("alphanum is AaBbCcDd" , ValidatorUtils.alphanum("AaBbCcDd"));
+        Assert.assertTrue("alphanum is ABCD1234" , ValidatorUtils.alphanum("ABCD1234"));
+        Assert.assertTrue("alphanum is AaBbCcDd1234" , ValidatorUtils.alphanum("AaBbCcDd1234"));
+        Assert.assertTrue("alphanum is      AaBbCcDd1234     " , ValidatorUtils.alphanum("    AaBbCcDd1234      "));
+        Assert.assertFalse("alphanum is AB CD" , ValidatorUtils.alphanum("AB CD"));
+        Assert.assertFalse("alphanum is ab cd" , ValidatorUtils.alphanum("ab cd"));
+        Assert.assertFalse("alphanum is ABCDㄱ" , ValidatorUtils.alphanum("ABCDㄱ"));
+        Assert.assertFalse("alphanum is AB CD" , ValidatorUtils.alphanum("ABCD"));
+    }
 }
